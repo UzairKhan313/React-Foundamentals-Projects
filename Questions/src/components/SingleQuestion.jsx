1ecf61a88e949d954677d783650e0469fca14796
UzairKhan313 <uzairkhaan2003@gmail.com>
2024-01-29 14:57:52 +0500

    final Commit

---
+++ Questions/src/components/SingleQuestion.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai'
+
+const SingleQuestion = ({ title, info }) => {
+  const [showInfo, setShowInfo] = useState(false)
+
+  return (
+    <article className="question">
+      <header>
+        {title}
+        <button className="question-btn" onClick={() => setShowInfo(!showInfo)}>
+          {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
+      </header>
+      {showInfo && <p>{info}</p>}
+    </article>
+  )
+}
+
+export default SingleQuestion
