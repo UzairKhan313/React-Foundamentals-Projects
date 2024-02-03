b3b192a1fb4666b7228b84c7c8f3f69dd911968b
UzairKhan313 <uzairkhaan2003@gmail.com>
2024-02-03 13:54:59 +0500

    Rendering menu list

--- Menu/src/App.jsx
+++ Menu/src/App.jsx
@@ -1,10 +1,15 @@
+import { useState } from 'react'
 import Title from './components/Title'
+import Menu from './components/Menu'
+import menu from './data'
 
 const App = () => {
+  const [menuItems, setMenuItems] = useState(menu)
   return (
     <main>
       <section className="menu">
         <Title text="Our Menu" />
+        <Menu items={menuItems} />
       </section>
     </main>
   )
