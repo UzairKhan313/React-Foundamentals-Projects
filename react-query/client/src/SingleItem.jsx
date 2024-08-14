f540f2b2b853c13175996c28a74cd31f730e942e
UzairKhan313 <uzairkhaan2003@gmail.com>
2024-08-15 00:19:25 +0500

    Done with react Query

---
+++ react-query/client/src/SingleItem.jsx
@@ -0,0 +1,59 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import customFetch from "./utils";
+import { toast } from "react-toastify";
+
+const SingleItem = ({ item }) => {
+  const queryClient = useQueryClient();
+  const { mutate: editTask } = useMutation({
+    mutationFn: ({ taskId, isDone }) => {
+      return customFetch.patch(`/${taskId}`, { isDone });
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["tasks"] });
+    },
+    onError: (error) => {
+      toast.error(error.response.data.msg);
+    },
+  });
+
+  const { mutate: deleteTask, isLoading } = useMutation({
+    mutationFn: (taskId) => {
+      return customFetch.delete(`/${taskId}`);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["tasks"] });
+    },
+    onError: (error) => {
+      toast.error(error.response.data.msg);
+    },
+  });
+
+  return (
+    <div className="single-item">
+      <input
+        type="checkbox"
+        checked={item.isDone}
+        onChange={() => editTask({ taskId: item.id, isDone: !item.isDone })}
+      />
+      <p
+        style={{
+          textTransform: "capitalize",
+          textDecoration: item.isDone && "line-through",
+        }}
+      >
+        {item.title}
+      </p>
+      <button
+        className="btn remove-btn"
+        type="button"
+        onClick={() => {
+          deleteTask(item.id);
+        }}
+        disabled={isLoading}
+      >
+        delete
+      </button>
+    </div>
+  );
+};
+export default SingleItem;
